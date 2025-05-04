5d17e1e35c90ffa104cb6d0cab2a7c93121a36a3
saminmahmud <samin.mahmud18@gmail.com>
2025-05-04 19:09:30 +0600

    change apiBaseUrl

--- src/apiBaseUrl/apiBaseUrl.js
+++ src/apiBaseUrl/apiBaseUrl.js
@@ -1,4 +1,4 @@
 // const apiBaseUrl = "http://127.0.0.1:8000";
-const apiBaseUrl = "https://quickfood-backend.up.railway.app";
+const apiBaseUrl = "https://quickfood-backend-3lxu.onrender.com";
 
 export default apiBaseUrl;
